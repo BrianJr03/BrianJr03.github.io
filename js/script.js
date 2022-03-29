9f6b97143288d85e77f8c3fe3566ff903d2a381b
Brian Walker Jr <55070185+BrianJr03@users.noreply.github.com>
2022-03-29 11:06:30 -0400

    removes unused js code

--- js/script.js
+++ js/script.js
@@ -32,20 +32,4 @@ $(document).ready(function() {
             }
         }
     });
-    var skillsTopOffset = $('.skillsSection').offset().top;
-    $(window).on('scroll', function() {
-        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-            $('.chart').easyPieChart({
-                easing: 'easeInOut',
-                barColor: '#FF69B4',
-                trackColor: false,
-                scaleColor: false,
-                lineWidth: 4,
-                size: 152,
-                onStep: function(from, to, percent) {
-                    $(this.el).find('.percent').text(Math.round(percent))
-                }
-            });
-        }
-    });
 });
