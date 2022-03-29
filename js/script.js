cb735207cf9cfa165894a1fe32a05b3adb3b8fc0
Brian Walker Jr <55070185+BrianJr03@users.noreply.github.com>
2022-03-29 16:33:06 -0400

    inits fancy box

--- js/script.js
+++ js/script.js
@@ -50,4 +50,13 @@ $(document).ready(function() {
             $('.aboutMeParagraph').fadeIn(1500);
         }
     });
+    $("[data-fancybox]").fancybox();
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    })
 });
