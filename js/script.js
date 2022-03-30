354a505219920cde583529648ec7ae2c7dd34099
Brian Walker Jr <55070185+BrianJr03@users.noreply.github.com>
2022-03-29 22:39:05 -0400

    adds webpage loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+$(window).on("load", function() {
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750)
+    });
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+});
 $(document).ready(function() {
     $('#slides').superslides(
         {
@@ -55,14 +68,6 @@ $(document).ready(function() {
         }
     });
     if (jQuery().fancybox) { $("[data-fancybox]").fancybox(); } 
-    $(".items").isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
-        }
-    });
     $("#filters a").click(function() {
         $("#filters .current").removeClass("current");
         $(this).addClass("current");
